1d8380acafaaa4f495c6aa09040b15ad0deb282c
Saraccostein <marco.vicp@tutanota.com>
2022-07-15 21:53:33 -0500

    Función dibujarLínea agregada

--- events.js
+++ events.js
@@ -1,3 +1,20 @@
+var paint = document.getElementById(área_de_dibujo);
+var lienzo = paint.getContext('2d');
+
+function dibujarLínea(color, x_inicial, y_inicial, anti_x_izq, anti_y_izq, anti_x_der, anti_y_der, x_final, y_final, lienzo)
+{
+    lienzo.beginPath();
+    lienzo.lineWidth = 4;
+    lienzo.lineCap ='round';
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(x_inicial, y_inicial);
+    /* lienzo.bezierCurveTo(anti_x_izq, anti_y_izq, anti_x_der, anti_y_der, x_final, y_final); */
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+dibujarLínea('#06EE9E', 250, 250, lienzo);
+
 var tecla = 
 {
     LEFT: 37,  /* ← */
